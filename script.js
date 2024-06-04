445e5f284a23b00754b6c9f52602c0ef7cb0d071
Wanasaurus <wanasaurus@hotmail.com>
2024-06-04 13:28:00 +0800

    Set style for a single square

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // One square
-let square = document.createElement("p");
-square.innerHTML = "Square";
+let square = document.createElement("div");
 document.querySelector(".gridSquares").appendChild(square);
-
+square.style.backgroundColor = "white";
+square.style.border = "1px solid black";
+square.style.height = "2rem";
+square.style.width = "2rem";
