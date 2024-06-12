d4afe6960c369fbb2bf9e04e65f56e1bbd2c879f
Wanasaurus <wanasaurus@hotmail.com>
2024-06-12 18:15:04 +0800

    Brainstorm ideas for function to create square

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 let square = document.createElement("div");
 document.querySelector(".gridSquares").appendChild(square);
 square.classList.add("smallSquare"); 
+
+
+//Make a square
+// function grid(side) {
+//     return side * side;
+// }
+
+// //Loop numbers
+// function loop(number) {
+//     for (let i = 1; i <= number; i++) {
+//         console.log(i + " " + "square");
+// }
+// } 
+
+// function loop(number) {
+//     for (let i = 1; i <= number; i++) {
+//         console.log("square");
+// }
+// }   
